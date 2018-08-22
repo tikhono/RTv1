393d80d59662b7daa22ccc51da1f0f04ee87c2b8
atikhono <tihonovandrey11@gmail.com>
2018-08-22 11:27:41 +0300

    delete opencl features

--- main.h
+++ main.h
@@ -6,7 +6,7 @@
 /*   By: atikhono <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/10 17:41:57 by atikhono          #+#    #+#             */
-/*   Updated: 2018/08/15 12:43:00 by atikhono         ###   ########.fr       */
+/*   Updated: 2018/08/22 11:25:32 by atikhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,14 @@
 # include <mlx.h>
 # include <math.h>
 # include <stdio.h>
-# include <OpenCL/cl.h>
 # include "./libft/libft.h"
-
-typedef struct			s_kernel
-{
-	cl_kernel			kernel;
-	cl_command_queue	queue;
-	cl_mem				input_buffer;
-	cl_mem				res_buffer;
-	size_t				global_size;
-	size_t				local_size;
-}						t_kernel;
 
 typedef struct			s_mlx
 {
 	void				*mlx;
 	void				*win;
 	void				*img;
+	int					*addr;
 }						t_mlx;
 
 typedef struct			s_data
@@ -58,16 +48,11 @@ typedef struct			s_all
 {
 	t_mlx				p;
 	t_data				d;
-	t_kernel			k;
-	int					*addr;
-	char				fix;
 }						t_all;
 
 int						call_hookers(int key, t_all *a);
 int						mouse_click(int key, int x, int y, t_all *a);
 int						mouse_move(int x, int y, t_all *a);
 int						exit_mouse(void);
-void					start_kernel(t_all *a);
-int						run_kernel(t_all *a);
 
 #endif
